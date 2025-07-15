5f8b5cccc21b6c214599a4e883b212dfc181d7b9
sulaman <Sulamanshahzad492@gmail.com>
2025-07-14 22:39:27 -0700

    variable in javascript

---
+++ Javascript/lecture 55/jsvariable.js
@@ -0,0 +1,17 @@
+let x=22;
+let y=2.2;
+let z="Harry bhai";
+let p= 'undefiined';
+let q= true;
+let s=null;
+
+console.log(x,y,z,p,q,s);
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof s)
+
+let o = {
+    "name":"harry",
+    "salary":"100 crore",
+    "is_handsome":true,
+}
+console.log(o)
+o.salary="300crore";
