11c699cc8a2105c6a8426fc105a9d583574a17b2
sulaman <Sulamanshahzad492@gmail.com>
2025-07-30 23:19:21 -0700

    starting express js

---
+++ node js/lecture 86/main.js
@@ -0,0 +1,22 @@
+// const { log } = require('node:console');
+// const http = require('node:http');
+
+// import http from 'http'
+// const hostname = '127.0.0.1';
+// const port = 3001;
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200; 
+//     res.setHeader('Content-Type', 'text/html');
+//     res.end('<h1>nefuidhnewuihfwehf  eeefwefew </h1>');
+// });
+// server.listen(port, hostname, () => {
+//     console.log(`Server running at http://${hostname}:${port}/`);
+
+// });
+
+import { a,b,c } from './mymodule.js'
+console.log(a,b,c);
+
+import obj from './mymodule.js'
+console.log(obj);
+
