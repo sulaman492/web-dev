5b44ac8d21972ab479be426a9e1d68fb33501c91
sulaman <Sulamanshahzad492@gmail.com>
2025-08-08 14:08:52 -0700

    backend of todo application

---
+++ todo/backend/routes/todo.route.js
@@ -0,0 +1,55 @@
+import express from "express"
+import todo from "../model/todo.model.js";
+
+
+const router=express.Router()
+
+router.get("/",async(req,res)=>{
+    try {
+        const todo=todo.find();
+        res.json(todo)
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }
+})
+
+router.post("/",async(req,res)=>{
+    const Todo=new todo({
+        text:req.body.text
+    })
+    try {
+        const newTodo=todo.save(Todo)
+        res.status(201).json(newTodo)
+    } catch (error) {
+        res.status(400).json({message:error.message})
+    } 
+})
+
+router.patch("/:id",async(req,res)=>{
+    try {
+        const newtodo=await todo.findById(req.params.id);
+        if(!newtodo) return res.status(400).json({message:"Todo not found"})
+
+        if(req.body.text!=undefined){
+            newtodo.text=req.body.text;
+        }    
+        if(req.body.completed!=undefined){
+            newtodo.completed=req.body.completed;
+        }
+        const updatedtodo=await newtodo.save();
+        res.json(updatedtodo)
+    } catch (error) {
+        res.status(400).json({message:error.message})
+    }
+})
+
+router.delete("/:id",async(req,res)=>{
+    try {
+        await todo.findByIdAndDelete(req.params.id);
+        res.json({message:"todo deleted"})
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }
+})
+
+export default router;
--- todo/backend/server.js
+++ todo/backend/server.js
@@ -1,7 +1,8 @@
 import express from "express"
-import connection, { connectDB } from "./config/db.js"
-import env from "dotenv";
-env.config();
+import { connectDB } from "./config/db.js"
+import todoRoutes from "./routes/todo.route.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app=express();
 app.use(express.json())
@@ -11,6 +12,7 @@ app.get("/",(req,res)=>{
     res.send("Hello world");
 })
 
+app.use("/api/routes",todoRoutes)
 app.listen(port,()=>{
     connectDB();
     console.log(`app listening on http://localhost:${port}`);
