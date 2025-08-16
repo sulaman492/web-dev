9e279a338296cc2e311b8d3b2efecccc432af6d4
sulaman <Sulamanshahzad492@gmail.com>
2025-08-15 22:51:59 -0700

    connecting mongodb with express

--- node js/lecture 96/mongodb.js
+++ node js/lecture 96/mongodb.js
@@ -1,14 +1,17 @@
 import express from "express";
 import mongoose from "mongoose";
+import { Todo } from "./models/Todo.js";
 
 const app = express();
 const port = 3000;
 
-mongoose.connect("mongodb://localhost:27017/mydb")
+mongoose.connect("mongodb://localhost:27017/todo")
   .then(() => console.log("Connected to MongoDB"))
   .catch(err => console.error("MongoDB connection error:", err));
 
 app.get("/", (req, res) => {
+    const todo=new Todo({title:"Hey first Todo",desc:"description of this first Todo",isDone:false})
+    todo.save();
   res.send("Hello World");
 });
 
